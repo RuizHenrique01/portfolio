13fd67f4bcfb0bb52c98fcf099aba8419ebce7c4
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2024-08-01 15:34:49 -0400

    feat: Updating curriculo file

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -5,7 +5,7 @@ import Header from '../../components/Header';
 import { Button, Card, CardContent, IconButton, Tooltip, Typography } from '@mui/material';
 import { Call, Engineering, GitHub, Instagram, LinkedIn, Mail, School, Terminal } from '@mui/icons-material';
 import { useEffect, useState } from 'react';
-import curriculoPDF from '../../assets/Curriculo - Affonso Ruiz 2024 - V2.pdf'
+import curriculoPDF from '../../assets/Curriculo v2.0 - Affonso Ruiz 2024.pdf'
 import skills from '../../mocks/skills';
 import projects from '../../mocks/projects';
 import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator, timelineItemClasses } from '@mui/lab';
